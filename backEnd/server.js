ea138415842da0a5630599c2692124886b53d93b
konrad <svahnkon@arcada.fi>
2023-01-16 10:17:00 +0200

    database updates correctly

--- backEnd/server.js
+++ backEnd/server.js
@@ -29,7 +29,7 @@ let temp
 
 //---------------------------------------------------------------------------------------------------------------
 
-// cheeking the database connection
+// checking the database connection
 database.on('error',(error) => console.log(error))
 database.on('open',() => console.log("connected to database "))
 
@@ -56,13 +56,17 @@ function getDroneInfo () {
     }
 }
 
+//getPilotInfo checks if a drone is in violation of the NDZ and gets the pilot information if it is 
 function getPilotInfo (parsed) { 
+    // distance is the distance between the drone and the birds nest in mm so a distance value of 100000 is 100m 
     let distance
     
+    // the code in this loop is run once for every drone in the current snapshot
     for (i = 0; i < parsed.drone.length; i++) {
         
-        // distance is the distance between the drone and the birds nest in mm so a distance value of 100000 is 100m 
+        // get distance calculates the distance to the nest based on the drones x and y coordinates 
         distance = getDistance(parsed.drone[i].positionX[0],parsed.drone[i].positionY[0])
+        console.log(distance)
 
         if (distance <= 100000) {
             try {
@@ -85,16 +89,74 @@ function getPilotInfo (parsed) {
             }
         }
     }
-
-    temp = parsed
 }
 
-function managePilot (pilot, time, distance) {
-    console.log(pilot)
-    console.log(time)
-    console.log(distance)
+//managePilot checks if a pilot is already in the database. updates them if they are ads them if they are not
+async function managePilot (pilot, time, distance) {
+    // dbInfo will contain the result of a get request for the pilot id made to the database
+    let dbInfo
+    // shortestDist is the shortest distance to the birds nest for the pilot
+    let shortestDist
+
+    try {
+        // dbInfo will be null if the pilot does not exist in the database and will return the pilot’s data if they do
+        dbInfo = await Model.findOne({pilotId: pilot.pilotId}).exec()
+        console.log(dbInfo)
+    
+        // if the pilot does not exist in te database they are added
+        if (dbInfo == null) {
+            try {
+                const model = new Model({
+                    pilotId: pilot.pilotId,
+                    firstName: pilot.firstName,
+                    lastName: pilot.lastName,
+                    retrieved: time,
+                    email: pilot.email,
+                    phoneNumber: pilot.phoneNumber,
+                    distance: distance
+                })
+                model.save()
+    
+            } catch (error) {
+                console.log(error)
+            }
+        
+        // if the pilot does exist in the database the distance is uppdated 
+        } else {
+            /* if the distance of the current violation (distance) is shorter than the 
+            shortest recorded distance (dbInfo.distance) it will become the new shortest distance
+            if not the shortest recorded distance will not change*/ 
+            if (distance < dbInfo.distance) {
+                shortestDist = distance
+            } else {
+                shortestDist = dbInfo.distance
+            }
+            
+            // the code below updates the pilot in the database 
+            try {
+                await Model.findOneAndUpdate(
+                    {pilotId: pilot.pilotId}, 
+                    {
+                        pilotId: pilot.pilotId,
+                        firstName: pilot.firstName,
+                        lastName: pilot.lastName,
+                        retrieved: time,
+                        email: pilot.email,
+                        phoneNumber: pilot.phoneNumber,
+                        distance: shortestDist
+                    }, 
+                    {new: true}
+                )
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
 } 
 
+//get distance calculates the distance to the nest based on the drones x and y coordinates 
 function getDistance (x,y) {
     let dx
     let dy
